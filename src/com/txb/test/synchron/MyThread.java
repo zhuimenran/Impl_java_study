b8a30a06371e4b1f52d6bb61e71897e30d360f74
zhuimenran <13125110952@163.com>
2019-05-22 19:14:47 +0800

    runable 与thread上区别

---
+++ src/com/txb/test/synchron/MyThread.java
@@ -0,0 +1,21 @@
+package com.txb.test.synchron;
+
+class MyThread extends Thread {
+    
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        synchronized(this) {
+            try {  
+                for (int i = 0; i < 5; i++) {
+                    Thread.sleep(100); // 休眠100ms
+                    System.out.println(Thread.currentThread().getName() + " loop " + i);  
+                }
+            } catch (InterruptedException ie) {  
+            }
+        }  
+    }
+}
